bf8d25a264c0f9ac14ebb160ed969bc9e7309efb
nikhils777 <nsharmatennis@gmail.com>
2014-05-30 10:39:22 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Welcome 
+_this sample application is a clone of reddit_
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RedditClone::Application.config.secret_key_base = '3a1941ada1965dded631a056196a2cce0f51932b483cbfba0647b5a28ac842a9c30bf5af91d5f2fedae32841b6aca61c01bcec3fb47c7689b6c403032287b415'
