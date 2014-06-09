b64489ca80fcd4ca3e84478c7c9b43e62dbbfd41
nikhils777 <nsharmatennis@gmail.com>
2014-06-09 16:52:40 -0400

    Implementing voting

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ RedditClone::Application.routes.draw do
   resources :topics do
     resources :posts, except: [:index] do
       resources :comments
+      get '/up-vote' => 'votes#up_vote', as: :up_vote
+      get '/down-vote' => 'votes#down_vote', as: :down_vote
     end
   end
   get 'about' => 'welcome#about'
