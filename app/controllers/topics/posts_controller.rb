d0f3b68fabb540ccaf11f69a20b3bf5094b7d5b6
nikhils777 <nsharmatennis@gmail.com>
2014-06-11 17:10:37 -0400

    Created Popular Posts and Users, and optimized topics#show with eager loading.

---
+++ app/controllers/topics/posts_controller.rb
@@ -0,0 +1,63 @@
+class Topics::PostsController < ApplicationController
+  def show
+    @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
+    authorize @topic
+    @comments = @post.comments
+    @comment = Comment.new
+  end
+
+  def new
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.new
+    authorize @post
+  end
+  def create
+    @topic = Topic.find(params[:topic_id])
+    @post = current_user.posts.build(post_params)
+    @post.topic = @topic
+    authorize @post
+    if @post.save
+      redirect_to [@topic, @post], notice: "Post was saved successfully."
+    else
+      flash[:error] = "Your post was not saved properly, please try again."
+      render :new
+    end
+  end
+  def edit
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+  end
+  def update
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Post Updated!"
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "The post was not updated successfully, please try again."
+      render :edit
+    end
+  end
+  def destroy
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    title = @post.title
+    authorize @post
+    if @post.destroy
+      flash[:notice] = "\"#{title}\" was deleted successfully."
+      redirect_to @topic
+    else
+      flash[:error] = "There was an error deleting the post."
+      render :show
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body, :postpic)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 RedditClone::Application.routes.draw do
+  get "posts/index"
   devise_for :users
-  resources :users, only: [:update, :show]
+  resources :users, only: [:update, :show, :index]
+  resources :posts, only: [:index]
   resources :topics do
-    resources :posts, except: [:index] do
+    resources :posts, except: [:index], controller: 'topics/posts' do
       resources :comments
       get '/up-vote' => 'votes#up_vote', as: :up_vote
       get '/down-vote' => 'votes#down_vote', as: :down_vote
